5cdb334bfed526baf64cab5e75b65186309af399
xnpst <xnpster@yandex.ru>
2022-02-07 09:16:34 +0300

    namespace

--- sem1/main.cpp
+++ sem1/main.cpp
@@ -1,1 +1,16 @@
-fasdf
+#include <iostream>
+    namespace A {
+        int x = 100;
+    };
+
+    namespace B {
+        int x = 200;
+    }
+
+int main() {
+    using std::cout;
+    using namespace A;
+    using namespace B;
+
+    cout << x << std::endl;
+}
